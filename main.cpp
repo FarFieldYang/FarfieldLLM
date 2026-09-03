8a3166e4216e7493497d63b7762998a68664652c
Zhiyuan Yang <311313904+FarFieldYang@users.noreply.github.com>
2026-09-03 15:17:16 -0700

    Byte_tokenizerTest

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include "byte_tokenizer.h"
 
 int main(){
-    std::cout << "hello, FarfieldLLM!" << std::endl;
+    std::string text = "hello, FarfieldLLM!  你好，远场大语言模型！😊😊😊";
+    ByteTokenizer tokenizer;
+    std::vector<int> tokens = tokenizer.encode(text);
+    std::string decode_text = tokenizer.decode(tokens);
+
+    std::cout << text << std::endl;
+    for (int token : tokens){
+        std::cout << token << ' ';
+    }
+    std::cout << std::endl;
+    std::cout << decode_text << std::endl;
     return 0;
 }
